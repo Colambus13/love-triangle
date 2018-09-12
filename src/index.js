af8f3004bad1703770dc3d02f65295aebf32798a
Pavel Shestak <pavelshestak29@gmail.com>
2018-09-13 02:37:06 +0300

    second

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var counter = 0;
+    var prev_first_lover,
+        pre_prev_first_lover,
+        pre_pre_prev_first_lover;
+
+    preferences.forEach(function(item, index, array) {
+
+        prev_first_lover = array[item - 1];
+        pre_prev_first_lover = array[prev_first_lover - 1];
+        pre_pre_prev_first_lover = pre_prev_first_lover - 1;
+
+        if (index === pre_pre_prev_first_lover) {
+            counter++;
+        }
+
+    });
+
+    return Math.trunc(counter/3);
 };
